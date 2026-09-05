56a590612d7e2421e0c50e5659041fc5de7f1d48
kamiltrach <99244756+kamiltrach@users.noreply.github.com>
2026-09-05 10:34:23 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,21 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyCSfpNxbRTwL2PEqRQrNW4UQ-B4zqhHkrVw",
+  authDomain: "strefa-f41ef.firebaseapp.com",
+  projectId: "strefa-f41ef",
+  storageBucket: "strefa-f41ef.firebasestorage.app",
+  messagingSenderId: "460055344104",
+  appId: "1:460055344104:web:221886736a2c85160ea5d",
+  measurementId: "G-ZPP3H30M35"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+window.db = firebase.firestore();
+window.auth = firebase.auth();
+
+// Opcjonalnie: drugi projekt Firebase dla A/B klas
+// window.db2 = firebase.initializeApp(SECOND_CONFIG, "secondary").firestore();
+window.db2 = window.db2 || null;
